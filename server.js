761e920efd4cd1b136c2d4c6b5c6a512078bc95e
aayman01 <ayman.alfi098@gmail.com>
2025-05-04 01:47:26 +0600

    all the scripts complete

---
+++ server.js
@@ -0,0 +1,12 @@
+// const fs = require('fs');
+// const https = require('https')
+const express = require("express");
+
+// const key = fs.readFileSync('./certs/cert.key')
+// const cert = fs.readFileSync('./certs/cert.crt')
+
+const app = express();
+// const secureExpress = https.createServer({key, cert}, app)
+
+app.use(express.static("public")); //serve our files in public statically
+app.listen(5000);
